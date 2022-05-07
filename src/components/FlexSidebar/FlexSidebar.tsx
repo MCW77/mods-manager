87c6d5b02820953103c141d606c717a3a149cbf9
MCW77 <michael.wisniewski77@gmail.com>
2022-05-07 20:07:08 +0200

    refactor: add FlexSidebar
    FlexSidebar can replace Sidebar component.
    Provides synchronous growing/shrinking of sidebar and main content.
    It is based on hooks.
    FlexSidebar is a flexbox container getting the sidebar content
    and main content as parameters.

---
+++ src/components/FlexSidebar/FlexSidebar.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import './FlexSidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+
+interface Props {
+  isCollapsed?: boolean;
+  mainContent: JSX.Element | JSX.Element[];
+  sidebarContent: JSX.Element | JSX.Element[];
+}
+
+const FlexSidebar = (props: Props) => {
+  const [t, i18n] = useTranslation('global-ui');
+  const [isCollapsed, setCollapsed] = useState(props.isCollapsed ?? false);
+
+  return (
+    <div className={`flexsidebar-container ${isCollapsed ? 'collapsed' : ''}`}>
+      <span
+        className={`flexsidebar-toggle ${isCollapsed ? 'show' : 'collapse'}`}
+        onClick={() => {
+          setCollapsed(!isCollapsed);
+        }}
+      >
+        <FontAwesomeIcon
+          icon={faAngleLeft}
+          title={`${isCollapsed ? t('sidebar.Toggle-show', 'show sidebar') : t(`sidebar.Toggle-collapse`, 'collapse sidebar')}`}
+        />
+      </span>
+      <div className={`flexsidebar`}>
+        {props.sidebarContent}
+      </div>
+      <div className={`flexsidebar-maincontent`}>
+        {props.mainContent}
+      </div>
+    </div>
+  );
+};
+
+export default FlexSidebar;
