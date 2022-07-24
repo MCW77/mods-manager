f57512eb8fe4c58cc1a615d3c141dcceeca1fa72
MCW77 <michael.wisniewski77@gmail.com>
2022-07-24 12:51:43 +0200

    refactor: replace class with functional component (using hooks)

--- src/components/ModLoadoutDetail/ModLoadoutDetail.tsx
+++ src/components/ModLoadoutDetail/ModLoadoutDetail.tsx
@@ -34,31 +34,164 @@ type PlayerStats = {
   [key in CSStats.GIMOStatNames | CSStats.CalculatedStatNames]: PlayerStat
 };
 
-class ModLoadoutDetail extends React.PureComponent<ComponentProps> {
-  render() {
-    const oldLoadout = this.props.oldLoadout;
-    const newLoadout = this.props.newLoadout;
-    const character = this.props.character;
-    const target = this.props.target;
-    const showAvatars = 'undefined' !== typeof this.props.showAvatars ? this.props.showAvatars : false;
-    const useUpgrades = this.props.useUpgrades;
-    const missedGoals = this.props.missedGoals || [];
+type ComponentProps = {
+  oldLoadout: ModLoadout;
+  newLoadout: ModLoadout;
+  showAvatars: boolean;
+  character: Character;
+  target: OptimizationPlan;
+  useUpgrades: boolean;
+  assignedCharacter: Character;
+  assignedTarget: OptimizationPlan;
+  missedGoals: MissedGoals;
+};
+
+
+const ModLoadoutDetail = React.memo(
+  ({
+    oldLoadout,
+    newLoadout,
+    showAvatars = false,
+    character,
+    target,
+    useUpgrades,
+    assignedCharacter,
+    assignedTarget,
+    missedGoals = [],
+  }: ComponentProps) => {
+    const addCalculatedStatsToPlayerValues = (playerStats: PlayerStat[], missedGoals: MissedGoals) => {
+      const currentStats = {
+        health: 0,
+        protection: 0,
+        armor: 0,
+        resistance: 0,
+        physDamage: 0,
+        specDamage: 0,
+        physCritChance: 0,
+        specCritChance: 0,
+        critDamage: 0,
+      };
+      const recommendedStats = {
+        health: 0,
+        protection: 0,
+        armor: 0,
+        resistance: 0,
+        physDamage: 0,
+        specDamage: 0,
+        physCritChance: 0,
+        specCritChance: 0,
+        critDamage: 0,
+      };
+
+      playerStats.forEach(stat => {
+        switch (stat.name) {
+          case 'Health':
+            currentStats['health'] = stat.currentValue;
+            recommendedStats['health'] = stat.recommendedValue;
+            break;
+          case 'Protection':
+            currentStats['protection'] = stat.currentValue;
+            recommendedStats['protection'] = stat.recommendedValue;
+            break;
+          case 'Armor':
+            currentStats['armor'] = stat.currentValue;
+            recommendedStats['armor'] = stat.recommendedValue;
+            break;
+          case 'Resistance':
+            currentStats['resistance'] = stat.currentValue;
+            recommendedStats['resistance'] = stat.recommendedValue;
+            break;
+          case 'Physical Damage':
+            currentStats['physDamage'] = stat.currentValue;
+            recommendedStats['physDamage'] = stat.recommendedValue;
+            break;
+          case 'Physical Critical Chance':
+            currentStats['physCritChance'] = stat.currentValue;
+            recommendedStats['physCritChance'] = stat.recommendedValue;
+            break;
+          case 'Special Damage':
+            currentStats['specDamage'] = stat.currentValue;
+            recommendedStats['specDamage'] = stat.recommendedValue;
+            break;
+          case 'Special Critical Chance':
+            currentStats['specCritChance'] = stat.currentValue;
+            recommendedStats['specCritChance'] = stat.recommendedValue;
+            break;
+          case 'Critical Damage':
+            currentStats['critDamage'] = stat.currentValue;
+            recommendedStats['critDamage'] = stat.recommendedValue;
+            break;
+          default:
+            break;
+        }
+      })
+
+      const currentEffectiveHealthPhysical =
+        (currentStats.health + currentStats.protection) / (1 - (currentStats.armor / 100));
+      const recommendedEffectiveHealthPhysical =
+        (recommendedStats.health + recommendedStats.protection) / (1 - (recommendedStats.armor / 100));
+      const currentEffectiveHealthSpecial =
+        (currentStats.health + currentStats.protection) / (1 - (currentStats.resistance / 100));
+      const recommendedEffectiveHealthSpecial =
+        (recommendedStats.health + recommendedStats.protection) / (1 - (recommendedStats.resistance / 100));
+      const currentAverageDamagePhysical = currentStats.physDamage *
+        ((1 - (currentStats.physCritChance / 100)) +
+          (currentStats.critDamage / 100) * (currentStats.physCritChance / 100)
+        );
+      const recommendedAverageDamagePhysical = recommendedStats.physDamage *
+        ((1 - (recommendedStats.physCritChance / 100)) +
+          (recommendedStats.critDamage / 100) * (recommendedStats.physCritChance / 100)
+        );
+      const currentAverageDamageSpecial = currentStats.specDamage *
+        ((1 - (currentStats.specCritChance / 100)) +
+          (currentStats.critDamage / 100) * (currentStats.specCritChance / 100)
+        );
+      const recommendedAverageDamageSpecial = recommendedStats.specDamage *
+        ((1 - (recommendedStats.specCritChance / 100)) +
+          (recommendedStats.critDamage / 100) * (recommendedStats.specCritChance / 100)
+        );
+
+      const statObject = (name: CSStats.CalculatedStatNames, currentValue: number, recommendedValue: number): PlayerStat => ({
+        name: name,
+        displayModifier: '',
+        currentValue: Math.floor(currentValue),
+        currentStat: null,
+        recommendedValue: Math.floor(recommendedValue),
+        optimizationValue: 0,
+        recommendedStat: null,
+        diffStat: new CSStats.CharacterSummaryStat(name, `${Math.floor(recommendedValue - currentValue)}`),
+        missedGoal: undefined,
+      });
+
+      playerStats.push(statObject(
+        'Effective Health (physical)',
+        currentEffectiveHealthPhysical,
+        recommendedEffectiveHealthPhysical
+      ));
+      playerStats.push(statObject(
+        'Effective Health (special)',
+        currentEffectiveHealthSpecial,
+        recommendedEffectiveHealthSpecial
+      ));
+      playerStats.push(statObject(
+        'Average Damage (physical)',
+        currentAverageDamagePhysical,
+        recommendedAverageDamagePhysical
+      ));
+      playerStats.push(statObject(
+        'Average Damage (special)',
+        currentAverageDamageSpecial,
+        recommendedAverageDamageSpecial
+      ));
+    }
+
 
     const newSummary = newLoadout.getSummary(character, target, useUpgrades);
     const oldSummary = oldLoadout.getSummary(character, target, false);
-    
+
     // Pull all the player's stats into an object that can be displayed without further calculation.
     const playerStats: PlayerStat[] = Object.values(newSummary).map( (stat: CSStats.CharacterSummaryStat) => {
       const diffStat = stat.minus(oldSummary[stat.type as CharacterStatNames.All]);
-/*
-      if (!character.playerValues.equippedStats || !stat) {
-        return {
-          name: stat.getDisplayType(),
-          recommendedValue: null,
-          diffStat: diffStat
-        };
-      }
-*/
       const statName: CharacterStatNames.All = stat.type as CharacterStatNames.All;
       let statValue = character.playerValues.equippedStats[statName] + stat.value;
 
@@ -103,7 +236,7 @@ class ModLoadoutDetail extends React.PureComponent<ComponentProps> {
     });
 
     // Add effective health and average damage to the stats display
-    this.addCalculatedStatsToPlayerValues(playerStats, missedGoals);
+    addCalculatedStatsToPlayerValues(playerStats, missedGoals);
 
     const statsDisplay = playerStats.map((stat, index) => {
       if (stat.recommendedValue == null) {
@@ -169,8 +302,12 @@ class ModLoadoutDetail extends React.PureComponent<ComponentProps> {
 
     return (
       <div className={'mod-set-detail'}>
-        <ModLoadoutView modLoadout={newLoadout} showAvatars={showAvatars} assignedCharacter={this.props.assignedCharacter}
-          assignedTarget={this.props.assignedTarget} />
+        <ModLoadoutView
+          modLoadout={newLoadout}
+          showAvatars={showAvatars}
+          assignedCharacter={assignedCharacter}
+          assignedTarget={assignedTarget}
+        />
         <div className={'summary'}>
           <table>
             <thead>
@@ -205,144 +342,8 @@ class ModLoadoutDetail extends React.PureComponent<ComponentProps> {
       </div>
     );
   }
+);
 
-  addCalculatedStatsToPlayerValues(playerStats: PlayerStat[], missedGoals: MissedGoals) {
-    const currentStats = {
-      health: 0,
-      protection: 0,
-      armor: 0,
-      resistance: 0,
-      physDamage: 0,
-      specDamage: 0,
-      physCritChance: 0,
-      specCritChance: 0,
-      critDamage: 0,
-    };
-    const recommendedStats = {
-      health: 0,
-      protection: 0,
-      armor: 0,
-      resistance: 0,
-      physDamage: 0,
-      specDamage: 0,
-      physCritChance: 0,
-      specCritChance: 0,
-      critDamage: 0,
-    };
-
-    playerStats.forEach(stat => {
-      switch (stat.name) {
-        case 'Health':
-          currentStats['health'] = stat.currentValue;
-          recommendedStats['health'] = stat.recommendedValue;
-          break;
-        case 'Protection':
-          currentStats['protection'] = stat.currentValue;
-          recommendedStats['protection'] = stat.recommendedValue;
-          break;
-        case 'Armor':
-          currentStats['armor'] = stat.currentValue;
-          recommendedStats['armor'] = stat.recommendedValue;
-          break;
-        case 'Resistance':
-          currentStats['resistance'] = stat.currentValue;
-          recommendedStats['resistance'] = stat.recommendedValue;
-          break;
-        case 'Physical Damage':
-          currentStats['physDamage'] = stat.currentValue;
-          recommendedStats['physDamage'] = stat.recommendedValue;
-          break;
-        case 'Physical Critical Chance':
-          currentStats['physCritChance'] = stat.currentValue;
-          recommendedStats['physCritChance'] = stat.recommendedValue;
-          break;
-        case 'Special Damage':
-          currentStats['specDamage'] = stat.currentValue;
-          recommendedStats['specDamage'] = stat.recommendedValue;
-          break;
-        case 'Special Critical Chance':
-          currentStats['specCritChance'] = stat.currentValue;
-          recommendedStats['specCritChance'] = stat.recommendedValue;
-          break;
-        case 'Critical Damage':
-          currentStats['critDamage'] = stat.currentValue;
-          recommendedStats['critDamage'] = stat.recommendedValue;
-          break;
-        default:
-          break;
-      }
-    })
-
-    const currentEffectiveHealthPhysical =
-      (currentStats.health + currentStats.protection) / (1 - (currentStats.armor / 100));
-    const recommendedEffectiveHealthPhysical =
-      (recommendedStats.health + recommendedStats.protection) / (1 - (recommendedStats.armor / 100));
-    const currentEffectiveHealthSpecial =
-      (currentStats.health + currentStats.protection) / (1 - (currentStats.resistance / 100));
-    const recommendedEffectiveHealthSpecial =
-      (recommendedStats.health + recommendedStats.protection) / (1 - (recommendedStats.resistance / 100));
-    const currentAverageDamagePhysical = currentStats.physDamage *
-      ((1 - (currentStats.physCritChance / 100)) +
-        (currentStats.critDamage / 100) * (currentStats.physCritChance / 100)
-      );
-    const recommendedAverageDamagePhysical = recommendedStats.physDamage *
-      ((1 - (recommendedStats.physCritChance / 100)) +
-        (recommendedStats.critDamage / 100) * (recommendedStats.physCritChance / 100)
-      );
-    const currentAverageDamageSpecial = currentStats.specDamage *
-      ((1 - (currentStats.specCritChance / 100)) +
-        (currentStats.critDamage / 100) * (currentStats.specCritChance / 100)
-      );
-    const recommendedAverageDamageSpecial = recommendedStats.specDamage *
-      ((1 - (recommendedStats.specCritChance / 100)) +
-        (recommendedStats.critDamage / 100) * (recommendedStats.specCritChance / 100)
-      );
-
-    const statObject = (name: CSStats.CalculatedStatNames, currentValue: number, recommendedValue: number): PlayerStat => ({
-      name: name,
-      displayModifier: '',
-      currentValue: Math.floor(currentValue),
-      currentStat: null,
-      recommendedValue: Math.floor(recommendedValue),
-      optimizationValue: 0,
-      recommendedStat: null,
-      diffStat: new CSStats.CharacterSummaryStat(name, `${Math.floor(recommendedValue - currentValue)}`),
-      missedGoal: undefined,
-    });
-
-    playerStats.push(statObject(
-      'Effective Health (physical)',
-      currentEffectiveHealthPhysical,
-      recommendedEffectiveHealthPhysical
-    ));
-    playerStats.push(statObject(
-      'Effective Health (special)',
-      currentEffectiveHealthSpecial,
-      recommendedEffectiveHealthSpecial
-    ));
-    playerStats.push(statObject(
-      'Average Damage (physical)',
-      currentAverageDamagePhysical,
-      recommendedAverageDamagePhysical
-    ));
-    playerStats.push(statObject(
-      'Average Damage (special)',
-      currentAverageDamageSpecial,
-      recommendedAverageDamageSpecial
-    ));
-  }
-}
-
-type ComponentProps = {
-  oldLoadout: ModLoadout,
-  newLoadout: ModLoadout,
-  showAvatars: boolean,
-  character: Character,
-  target: OptimizationPlan,
-  useUpgrades: boolean,
-  assignedCharacter: Character,
-  assignedTarget: OptimizationPlan,
-  missedGoals: MissedGoals,
-}
+ModLoadoutDetail.displayName = 'ModLoadoutDetail';
 
 export { ModLoadoutDetail };
