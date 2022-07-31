6fc50552fbf0223435c51aa26450d7c0c2f74719
MCW77 <michael.wisniewski77@gmail.com>
2022-07-31 02:50:05 +0200

    refactor: add memoization to Dropdown component

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,24 +1,30 @@
 // react
-import React from "react";
+import React from 'react';
 
 // styles
-import './Dropdown.css'
+import './Dropdown.css';
 
+type SelectProps = React.HTMLProps<HTMLSelectElement>;
 
-type SelectProps = React.HTMLProps<HTMLSelectElement>
-
-type DropdownProps = {
-    name?: string,
-    defaultValue?: string | number,
-    onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void
+type ComponentProps = {
+  name?: string;
+  defaultValue?: string | number;
+  onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 } & SelectProps;
 
-const Dropdown = React.forwardRef<HTMLSelectElement, DropdownProps>(
-    function Dropdown(props, ref) {
-        return <div className={'dropdown'}>
-            <select {...props} ref={ref} />
-        </div>;
-    }
+const Dropdown = React.memo(
+  React.forwardRef<HTMLSelectElement, ComponentProps>(function Dropdown(
+    props,
+    ref,
+  ) {
+    return (
+      <div className={'dropdown'}>
+        <select {...props} ref={ref} />
+      </div>
+    );
+  }),
 );
+
+Dropdown.displayName = 'Dropdown';
 
 export { Dropdown };
