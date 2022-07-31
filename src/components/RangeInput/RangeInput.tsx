e4e13a03eb4bed3e2d0b054aa3d2bada4c1c1fac
MCW77 <michael.wisniewski77@gmail.com>
2022-07-31 03:17:12 +0200

    refactor: replace class with functional component (using hooks)

--- src/components/RangeInput/RangeInput.tsx
+++ src/components/RangeInput/RangeInput.tsx
@@ -1,104 +1,8 @@
 // react
-import React from 'react';
-import { connect, ConnectedProps } from "react-redux";
-import { ThunkDispatch } from '::7../state/reducers/modsOptimizer';
+import React, { useRef } from 'react';
 
 // styles
 import './RangeInput.css';
-
-// state
-import { IAppState } from '../../state/storage';
-
-
-class RangeInput extends React.PureComponent<Props> {
-  slider: React.RefObject<HTMLInputElement>;
-  textfield: React.RefObject<HTMLInputElement>;
-  output: React.RefObject<HTMLOutputElement>;
-  
-  constructor(props: Props) {
-    super(props);
-    this.slider = React.createRef<HTMLInputElement>();
-    this.textfield = React.createRef<HTMLInputElement>();
-    this.output = React.createRef<HTMLOutputElement>();
-  }
-
-  render() {
-    const id = this.props.id;
-    const name = this.props.name;
-    const defaultValue = this.props.defaultValue;
-    const min = this.props.min || 0;
-    const max = this.props.max || 100;
-    const step = 'undefined' === typeof this.props.step ? 1 : this.props.step;
-    const isPercent = this.props.isPercent || false;
-    const editable = this.props.editable || false;
-    const onChange = this.props.onChange || function () { };
-    
-    if (editable) {
-      return [
-        <input
-          type={'range'}
-          id={name && name + '-slider'}
-          defaultValue={defaultValue}
-          min={min}
-          max={max}
-          step={step}
-          onChange={(e) => {
-            if (this.textfield.current !== null) {
-              this.textfield.current.value = e.target.value;
-              onChange(+e.target.value);
-            }
-          }}
-          key={name + 'slider'}
-          ref={this.slider}
-        />,
-        <input
-          type={'number'}
-          id={id}
-          name={name}
-          className={'slider-input'}
-          defaultValue={defaultValue}
-          min={min}
-          max={max}
-          step={step}
-          onChange={(e) => {
-            if (this.slider.current !== null) {
-              this.slider.current.value = e.target.value;
-              onChange(+e.target.value);
-            }
-          }}
-          key={name + 'input'}
-          ref={this.textfield}
-        />,
-        <span key={'percent'}>{isPercent && '%'}</span>
-      ];
-    } else {
-      return [
-        <input
-          type={'range'}
-          id={id}
-          name={name}
-          defaultValue={defaultValue}
-          min={min}
-          max={max}
-          step={step}
-          onChange={(e) => {
-            if (this.output.current !== null) {
-              this.output.current.value = e.target.value + (isPercent ? '%' : '');
-              onChange(+e.target.value);
-            }
-          }}
-          key={name + 'input'}
-        />,
-        <output id={name && name + '-display'} htmlFor={id} key={name + 'output'} ref={this.output}>
-          {defaultValue}{isPercent && '%'}
-        </output>
-      ];
-    }
-  }
-}
-
-type Props = PropsFromRedux & ComponentProps;
-type PropsFromRedux = ConnectedProps<typeof connector>;
 
 type ComponentProps = {
   id: string;
@@ -112,14 +16,90 @@ type ComponentProps = {
   onChange?: (value: number) => void;
 }
 
+const RangeInput = React.memo(
+  ({
+    id,
+    name,
+    defaultValue,
+    min = 0,
+    max = 100,
+    step = 1,
+    isPercent = false,
+    editable = false,
+    onChange = () => {},
+  }: ComponentProps) => {
+    const slider = useRef<HTMLInputElement>(null);
+    const textfield = useRef<HTMLInputElement>(null);
+    const output = useRef<HTMLOutputElement>(null);    
 
-const mapStateToProps = (state: IAppState) => {
-  return {}
-}
+    if (editable) {
+      return (
+        <>
+          <input
+            type={'range'}
+            id={name && name + '-slider'}
+            defaultValue={defaultValue}
+            min={min}
+            max={max}
+            step={step}
+            onChange={(e) => {
+              if (textfield.current !== null) {
+                textfield.current.value = e.target.value;
+                onChange(+e.target.value);
+              }
+            }}
+            key={name + 'slider'}
+            ref={slider}
+          />,
+          <input
+            type={'number'}
+            id={id}
+            name={name}
+            className={'slider-input'}
+            defaultValue={defaultValue}
+            min={min}
+            max={max}
+            step={step}
+            onChange={(e) => {
+              if (slider.current !== null) {
+                slider.current.value = e.target.value;
+                onChange(+e.target.value);
+              }
+            }}
+            key={name + 'input'}
+            ref={textfield}
+          />,
+          <span key={'percent'}>{isPercent && '%'}</span>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <input
+            type={'range'}
+            id={id}
+            name={name}
+            defaultValue={defaultValue}
+            min={min}
+            max={max}
+            step={step}
+            onChange={(e) => {
+              if (output.current !== null) {
+                output.current.value = e.target.value + (isPercent ? '%' : '');
+                onChange(+e.target.value);
+              }
+            }}
+            key={name + 'input'}
+          />,
+          <output id={name && name + '-display'} htmlFor={id} key={name + 'output'} ref={output}>
+            {defaultValue}{isPercent && '%'}
+          </output>
+        </>
+      );
+    }
+  }
+);
 
-const mapDispatchToProps = (dispatch: ThunkDispatch) => ({
-})
+RangeInput.displayName = 'RangeInput';
 
-let connector = connect(mapStateToProps, mapDispatchToProps);
-
-export default connector(RangeInput);
+export default RangeInput;
