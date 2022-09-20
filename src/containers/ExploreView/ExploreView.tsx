d41316f298ac2968fc980d95e648154161946c50
MCW77 <michael.wisniewski77@gmail.com>
2022-09-20 15:26:53 +0200

    refactor: extract ModsFilter class to own File

--- src/containers/ExploreView/ExploreView.tsx
+++ src/containers/ExploreView/ExploreView.tsx
@@ -1,31 +1,28 @@
 // react
-import React from "react";
-import { withTranslation, WithTranslation } from "react-i18next";
-import { connect, ConnectedProps } from "react-redux";
-import { ThunkDispatch } from "../../state/reducers/modsOptimizer";
+import React from 'react';
+import { withTranslation, WithTranslation } from 'react-i18next';
+import { connect, ConnectedProps } from 'react-redux';
+import { ThunkDispatch } from '../../state/reducers/modsOptimizer';
 
 // styles
 import {
   faAnglesDown,
   faAnglesUp,
   faTrashCan,
-} from "@fortawesome/free-solid-svg-icons";
-import "./ExploreView.css";
+} from '@fortawesome/free-solid-svg-icons';
+import './ExploreView.css';
 
 // utils
-import { Dictionary } from "lodash";
-import { groupBy } from "../../utils/groupBy";
-import memoizeOne from "memoize-one";
-import { orderBy, mapValues, forEach } from "lodash-es";
+import { forEach } from 'lodash-es';
 
 // state
-import { IAppState } from "../../state/storage";
+import { IAppState } from '../../state/storage';
 
 // actions
 import {
   hideModal,
   showModal,
-} from "../../state/actions/app";
+} from '../../state/actions/app';
 
 // thunks
 import {
@@ -33,34 +30,25 @@ import {
 } from '../../state/thunks/storage';
 
 // domain
-import { CharacterNames } from "../../constants/characterSettings";
-import {
-  EquippedSettings,
-  FilterKeys,
-  FilterOptions,
-  LevelSettings,
-  ModsViewOptions,
-  PrimarySettings,
-  RaritySettings,
-  SecondariesScoreTierSettings,
-  SecondarySettings,
-  SetSettings,
-  SlotSettings,
-  TierSettings,
-} from "../../domain/types/ModsViewOptionsTypes";
-import { Characters } from "../../domain/Character";
-import { Mod } from "../../domain/Mod";
-import { OptimizerSettings } from "../../domain/OptimizerSettings";
+import { CharacterNames } from '../../constants/characterSettings';
+import { ModsFilter } from '../../modules/modExploration/domain/ModsFilter';
+
+import { Characters } from '../../domain/Character';
+import { Mod } from '../../domain/Mod';
 
 // components
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { DOMContent } from "../../components/types";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { DOMContent } from '../../components/types';
 
-import { FlexSidebar } from "../../components/FlexSidebar/FlexSidebar";
-import { ModDetail } from "../../components/ModDetail/ModDetail";
-import ModFilter from "../../components/ModFilter/ModFilter";
-import { RenderIfVisible } from "../../components/RenderIfVisible/RenderIfVisible";
+import { FlexSidebar } from '../../components/FlexSidebar/FlexSidebar';
+import { ModDetail } from '../../components/ModDetail/ModDetail';
+import ModFilter from '../../components/ModFilter/ModFilter';
+import { RenderIfVisible } from '../../components/RenderIfVisible/RenderIfVisible';
 
+
+type AssignedMods = {
+  [key: string]: CharacterNames
+}
 
 class ExploreView extends React.PureComponent<Props> {
   modGroupToggle = (e: React.MouseEvent<HTMLDivElement>) => {
@@ -227,195 +215,6 @@ class ExploreView extends React.PureComponent<Props> {
         <ModFilter />
       </div>
     );
-  }
-}
-
-type AssignedMods = {
-  [key: string]: CharacterNames
-}
-type AnyFilterOptions = {
-  [key in keyof FilterOptions]: any[]
-};
-
-class ModsFilter {
-
-  selectedOptions: AnyFilterOptions;
-  unselectedOptions: AnyFilterOptions;
-
-  sortOptions: string[];
-  isGroupingEnabled: boolean;
-
-  constructor(modsViewOptions: ModsViewOptions) {
-    Mod.setupAccessors();
-    [this.selectedOptions, this.unselectedOptions] = this.extractSelectedAndUnselectedOptions(modsViewOptions.filtering);    
-    
-    this.isGroupingEnabled = modsViewOptions.isGroupingEnabled;
-
-    this.sortOptions = modsViewOptions.sort;
-    this.sortOptions = this.sortOptions.filter((option) => option !== '');
-    
-    if (!this.sortOptions.includes("character")) {
-      this.sortOptions.push("character");
-      this.sortOptions.push("characterID");
-    }
-  }
-
-  extractSelectedAndUnselectedOptions(filters: FilterOptions)
-  {
-    const selectedOptions: { [key in FilterKeys]: any[] } = {
-      slot: [],
-      set: [],
-      rarity: [],
-      tier: [],
-      level: [],
-      equipped: [],
-      primary: [],
-      secondary: [],
-      optimizer: [],
-      secondariesscoretier: [],
-    };
-    const unselectedOptions: { [key in FilterKeys]: any[] } = {
-      slot: [],
-      set: [],
-      rarity: [],
-      tier: [],
-      level: [],
-      equipped: [],
-      primary: [],
-      secondary: [],
-      optimizer: [],
-      secondariesscoretier: [],
-    };
-  
-    // #region CombinedSettings
-    type CombinedSettings =
-      | SlotSettings
-      | SetSettings
-      | RaritySettings
-      | TierSettings
-      | LevelSettings
-      | PrimarySettings
-      | SecondarySettings
-      | EquippedSettings
-      | OptimizerSettings
-      | SecondariesScoreTierSettings;
-    // #endregion
-  
-    type FilterKV = [FilterKeys, CombinedSettings];
-  
-    (Object.entries(filters) as FilterKV[])
-    .forEach(([type, values]) => {
-      selectedOptions[type] = Object.entries(values)
-        .filter(([option, value]) => 1 === value)
-        .map(([option]) => (isNaN(Number(option)) ? option : +option));
-      unselectedOptions[type] = Object.entries(values)
-        .filter(([option, value]) => -1 === value)
-        .map(([option]) => (isNaN(Number(option)) ? option : +option));
-    });
-  
-    return [selectedOptions, unselectedOptions];
-  }
-   
-  selectedOptionsFilter = (mod: Mod) => {
-    if (this.selectedOptions.slot.length > 0 && !this.selectedOptions.slot.every(slot => mod.slot === slot))
-      return false;
-    if (this.selectedOptions.set.length > 0 && !this.selectedOptions.set.every(set => mod.set === set))
-      return false;
-    if (this.selectedOptions.rarity.length > 0 && !this.selectedOptions.rarity.every(pips => mod.pips === pips))
-      return false;
-    if (this.selectedOptions.tier.length > 0 && !this.selectedOptions.tier.every(tier => mod.tier === tier))
-      return false;
-    if (this.selectedOptions.level.length > 0 && !this.selectedOptions.level.every(level => mod.level === level))
-      return false;
-    if (this.selectedOptions.equipped.length > 0 && mod.characterID === 'null')
-      return false;
-    if (this.selectedOptions.primary.length > 0 && !this.selectedOptions.primary.every(primary => mod.primaryStat.type === primary))
-      return false;
-    if (this.selectedOptions.secondary.length > 0 && !this.selectedOptions.secondary.every(secondary => mod.secondaryStats.some(modSecondary => modSecondary.type === secondary)))
-      return false;
-    if (this.selectedOptions.optimizer.length > 0 && !mod.isAssigned())
-      return false;
-    return true;
-  };
-
-  unselectedOptionsFilter = (mod: Mod) => {
-    if (this.unselectedOptions.slot.length > 0 && !this.unselectedOptions.slot.every(slot => mod.slot !== slot))
-      return false;
-    if (this.unselectedOptions.set.length > 0 && !this.unselectedOptions.set.every(set => mod.set !== set))
-      return false;
-    if (this.unselectedOptions.rarity.length > 0 && !this.unselectedOptions.rarity.every(pips => mod.pips !== pips))
-      return false;
-    if (this.unselectedOptions.tier.length > 0 && !this.unselectedOptions.tier.every(tier => mod.tier !== tier))
-      return false;
-    if (this.unselectedOptions.level.length > 0 && !this.unselectedOptions.level.every(level => mod.level !== level))
-      return false;
-    if (this.unselectedOptions.equipped.length > 0 && mod.characterID !== 'null')
-      return false;
-    if (this.unselectedOptions.primary.length > 0 && !this.unselectedOptions.primary.every(primary => mod.primaryStat.type !== primary))
-      return false;
-    if (this.unselectedOptions.secondary.length > 0 && !this.unselectedOptions.secondary.every(secondary => mod.secondaryStats.every(modSecondary => modSecondary.type !== secondary)))
-      return false;
-    if (this.unselectedOptions.optimizer.length > 0 && mod.isAssigned())
-      return false;
-    return true;
-  };
-    
-  filterMods(mods: Mod[]) {
-    return mods.filter(this.selectedOptionsFilter).filter(this.unselectedOptionsFilter)
-  }
-
-  filterGroupedMods(groupedMods: Dictionary<Mod[]>): Dictionary<Mod[]> {
-    let filteredMods: Dictionary<Mod[]> = mapValues(groupedMods, (mods: Mod[]) => 
-      this.filterMods(mods)
-    );
-    for (let group in filteredMods) {
-      if (filteredMods[group].length === 0)
-      delete filteredMods[group]  
-    }
-    return filteredMods;
-  }
-
-  groupMods(mods: Mod[]) {
-    const ungroupedMods = memoizeOne((mods: Mod[]) => {
-      return {
-        all: mods
-      }
-    });
-
-    const groupedMods = memoizeOne((mods: Mod[]) => {
-      return groupBy(
-        mods,
-        (mod: Mod) => mod.slot + "-" + mod["set"] + "-" + mod.primaryStat.type
-      );
-    });
-
-    if (this.isGroupingEnabled)
-      return groupedMods(mods)
-    else
-      return ungroupedMods(mods); 
-  }
-
-  sortGroupedMods(groupedMods: Dictionary<Mod[]>): Dictionary<Mod[]> {
-    return mapValues(groupedMods, (mods: Mod[]) => 
-      orderBy(
-        mods,
-        this.sortOptions,
-        this.sortOptions.map((opt) => "desc")
-      )
-    )
-  }
-
-  getGroupedModsCount(groupedMods: Dictionary<Mod[]>) {
-    return Object.entries(groupedMods).reduce((acc, [group, mods]) => {
-      return acc + mods.length;
-    }, 0)
-  }
-
-  applyModsViewOptions(mods: Mod[]): [Dictionary<Mod[]>, number] {
-    let groupedMods = this.groupMods(mods);
-    groupedMods = this.filterGroupedMods(groupedMods);
-    groupedMods = this.sortGroupedMods(groupedMods);
-    return [groupedMods, this.getGroupedModsCount(groupedMods)];
   }
 }
 
---
+++ src/modules/modExploration/domain/ModsFilter.ts
@@ -0,0 +1,212 @@
+// utils
+import { Dictionary } from "lodash";
+import { groupBy } from "../../../utils/groupBy";
+import memoizeOne from "memoize-one";
+import { orderBy, mapValues } from "lodash-es";
+
+// domain
+import {
+  EquippedSettings,
+  FilterKeys,
+  FilterOptions,
+  LevelSettings,
+  ModsViewOptions,
+  PrimarySettings,
+  RaritySettings,
+  SecondariesScoreTierSettings,
+  SecondarySettings,
+  SetSettings,
+  SlotSettings,
+  TierSettings,
+} from "../../../domain/types/ModsViewOptionsTypes";
+import { Mod } from "../../../domain/Mod";
+import { OptimizerSettings } from "../../../domain/OptimizerSettings";
+
+type AnyFilterOptions = {
+  [key in keyof FilterOptions]: any[]
+};
+
+class ModsFilter {
+
+  selectedOptions: AnyFilterOptions;
+  unselectedOptions: AnyFilterOptions;
+
+  sortOptions: string[];
+  isGroupingEnabled: boolean;
+
+  constructor(modsViewOptions: ModsViewOptions) {
+    Mod.setupAccessors();
+    [this.selectedOptions, this.unselectedOptions] = this.extractSelectedAndUnselectedOptions(modsViewOptions.filtering);    
+
+    this.isGroupingEnabled = modsViewOptions.isGroupingEnabled;
+
+    this.sortOptions = modsViewOptions.sort;
+    this.sortOptions = this.sortOptions.filter((option) => option !== '');
+
+    if (!this.sortOptions.includes("character")) {
+      this.sortOptions.push("character");
+      this.sortOptions.push("characterID");
+    }
+  }
+
+  extractSelectedAndUnselectedOptions(filters: FilterOptions)
+  {
+    const selectedOptions: { [key in FilterKeys]: any[] } = {
+      slot: [],
+      set: [],
+      rarity: [],
+      tier: [],
+      level: [],
+      equipped: [],
+      primary: [],
+      secondary: [],
+      optimizer: [],
+      secondariesscoretier: [],
+    };
+    const unselectedOptions: { [key in FilterKeys]: any[] } = {
+      slot: [],
+      set: [],
+      rarity: [],
+      tier: [],
+      level: [],
+      equipped: [],
+      primary: [],
+      secondary: [],
+      optimizer: [],
+      secondariesscoretier: [],
+    };
+
+    // #region CombinedSettings
+    type CombinedSettings =
+      | SlotSettings
+      | SetSettings
+      | RaritySettings
+      | TierSettings
+      | LevelSettings
+      | PrimarySettings
+      | SecondarySettings
+      | EquippedSettings
+      | OptimizerSettings
+      | SecondariesScoreTierSettings
+    ;
+    // #endregion
+
+    type FilterKV = [FilterKeys, CombinedSettings];
+
+    (Object.entries(filters) as FilterKV[])
+    .forEach(([type, values]) => {
+      selectedOptions[type] = Object.entries(values)
+        .filter(([option, value]) => 1 === value)
+        .map(([option]) => (isNaN(Number(option)) ? option : +option));
+      unselectedOptions[type] = Object.entries(values)
+        .filter(([option, value]) => -1 === value)
+        .map(([option]) => (isNaN(Number(option)) ? option : +option));
+    });
+
+    return [selectedOptions, unselectedOptions];
+  }
+   
+  selectedOptionsFilter = (mod: Mod) => {
+    if (this.selectedOptions.slot.length > 0 && !this.selectedOptions.slot.every(slot => mod.slot === slot))
+      return false;
+    if (this.selectedOptions.set.length > 0 && !this.selectedOptions.set.every(set => mod.set === set))
+      return false;
+    if (this.selectedOptions.rarity.length > 0 && !this.selectedOptions.rarity.every(pips => mod.pips === pips))
+      return false;
+    if (this.selectedOptions.tier.length > 0 && !this.selectedOptions.tier.every(tier => mod.tier === tier))
+      return false;
+    if (this.selectedOptions.level.length > 0 && !this.selectedOptions.level.every(level => mod.level === level))
+      return false;
+    if (this.selectedOptions.equipped.length > 0 && mod.characterID === 'null')
+      return false;
+    if (this.selectedOptions.primary.length > 0 && !this.selectedOptions.primary.every(primary => mod.primaryStat.type === primary))
+      return false;
+    if (this.selectedOptions.secondary.length > 0 && !this.selectedOptions.secondary.every(secondary => mod.secondaryStats.some(modSecondary => modSecondary.type === secondary)))
+      return false;
+    if (this.selectedOptions.optimizer.length > 0 && !mod.isAssigned())
+      return false;
+    return true;
+  };
+
+  unselectedOptionsFilter = (mod: Mod) => {
+    if (this.unselectedOptions.slot.length > 0 && !this.unselectedOptions.slot.every(slot => mod.slot !== slot))
+      return false;
+    if (this.unselectedOptions.set.length > 0 && !this.unselectedOptions.set.every(set => mod.set !== set))
+      return false;
+    if (this.unselectedOptions.rarity.length > 0 && !this.unselectedOptions.rarity.every(pips => mod.pips !== pips))
+      return false;
+    if (this.unselectedOptions.tier.length > 0 && !this.unselectedOptions.tier.every(tier => mod.tier !== tier))
+      return false;
+    if (this.unselectedOptions.level.length > 0 && !this.unselectedOptions.level.every(level => mod.level !== level))
+      return false;
+    if (this.unselectedOptions.equipped.length > 0 && mod.characterID !== 'null')
+      return false;
+    if (this.unselectedOptions.primary.length > 0 && !this.unselectedOptions.primary.every(primary => mod.primaryStat.type !== primary))
+      return false;
+    if (this.unselectedOptions.secondary.length > 0 && !this.unselectedOptions.secondary.every(secondary => mod.secondaryStats.every(modSecondary => modSecondary.type !== secondary)))
+      return false;
+    if (this.unselectedOptions.optimizer.length > 0 && mod.isAssigned())
+      return false;
+    return true;
+  };
+
+  filterMods(mods: Mod[]) {
+    return mods.filter(this.selectedOptionsFilter).filter(this.unselectedOptionsFilter)
+  }
+
+  filterGroupedMods(groupedMods: Dictionary<Mod[]>): Dictionary<Mod[]> {
+    let filteredMods: Dictionary<Mod[]> = mapValues(groupedMods, (mods: Mod[]) => 
+      this.filterMods(mods)
+    );
+    for (let group in filteredMods) {
+      if (filteredMods[group].length === 0)
+      delete filteredMods[group]
+    }
+    return filteredMods;
+  }
+
+  groupMods(mods: Mod[]) {
+    const ungroupedMods = memoizeOne((mods: Mod[]) => {
+      return {
+        all: mods
+      }
+    });
+
+    const groupedMods = memoizeOne((mods: Mod[]) => {
+      return groupBy(
+        mods,
+        (mod: Mod) => mod.slot + "-" + mod["set"] + "-" + mod.primaryStat.type
+      );
+    });
+
+    if (this.isGroupingEnabled)
+      return groupedMods(mods)
+    else
+      return ungroupedMods(mods); 
+  }
+
+  sortGroupedMods(groupedMods: Dictionary<Mod[]>): Dictionary<Mod[]> {
+    return mapValues(groupedMods, (mods: Mod[]) => 
+      orderBy(
+        mods,
+        this.sortOptions,
+        this.sortOptions.map((opt) => "desc"),
+      )
+    )
+  }
+
+  getGroupedModsCount(groupedMods: Dictionary<Mod[]>) {
+    return Object.entries(groupedMods).reduce((acc, [group, mods]) => {
+      return acc + mods.length;
+    }, 0)
+  }
+
+  applyModsViewOptions(mods: Mod[]): [Dictionary<Mod[]>, number] {
+    let groupedMods = this.groupMods(mods);
+    groupedMods = this.filterGroupedMods(groupedMods);
+    groupedMods = this.sortGroupedMods(groupedMods);
+    return [groupedMods, this.getGroupedModsCount(groupedMods)];
+  }
+}
+
+export { ModsFilter };
