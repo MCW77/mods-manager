7c1c0d9ef9882848512f23ca1b3dbe40b34c1046
MCW77 <michael.wisniewski77@gmail.com>
2023-12-30 22:52:44 +0100

    refactor: change parameter to avoid redux dependency

--- src/utils/readFile.ts
+++ src/utils/readFile.ts
@@ -1,26 +1,24 @@
-// react
-import { ThunkDispatch } from '../state/reducers/modsOptimizer';
-
-// modules
-import { App } from "#/state/modules/app";
-
 /**
  * Read a file as input and pass its contents to another function for processing
  * @param fileInput The uploaded file
  * @param handleResult Function string => *
- * @param dispatch ThunkDispatch
+ * @param handleError Function Error => *
  */
-export const readFile = (fileInput: Blob, handleResult: (textInFile: string) => void, dispatch: ThunkDispatch) => {
-  const reader = new FileReader();
+export const readFile = (
+	fileInput: Blob,
+	handleSuccess: (textInFile: string) => void,
+	handleError: (e: Error) => void,
+) => {
+	const reader = new FileReader();
 
-  reader.onload = (event) => {
-    try {
-      const fileData: string = event?.target?.result as string ?? '';
-      handleResult(fileData);
-    } catch (e) {
-      dispatch(App.actions.showError((e as Error).message));
-    }
-  };
+	reader.onload = (event) => {
+		try {
+			const fileData: string = (event?.target?.result as string) ?? "";
+			handleSuccess(fileData);
+		} catch (e) {
+			handleError(e as Error);
+		}
+	};
 
-  reader.readAsText(fileInput);
-}
+	reader.readAsText(fileInput);
+};
