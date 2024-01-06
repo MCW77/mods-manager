a7ba79a108d38c8e31d935c991bf0155fda3260a
MCW77 <michael.wisniewski77@gmail.com>
2024-01-06 17:21:37 +0100

    refactor: add HelpLink component
    Click to jump to a section and topic in the helppages given for this link

---
+++ src/components/HelpLink/HelpLink.tsx
@@ -0,0 +1,43 @@
+// react
+import React from "react";
+import { useDispatch } from "react-redux";
+
+// modules
+import { Help } from '#/state/modules/help';
+
+//domain
+import { HelpSections } from "#/domain/HelpSections";
+
+// components
+import { HelpCircle } from "lucide-react";
+
+import { Button } from "#ui/button";
+
+
+type ComponentProps = {
+  title: string;
+  section: HelpSections;
+  topic: number;
+}
+
+const HelpLink = React.memo(
+  ({title, section, topic}: ComponentProps) => {
+    const dispatch = useDispatch();
+
+    return <Button
+      className='inline-block h-6 w-6 m-x-1 m-y-0 align-middle cursor-pointer'
+      size='icon'
+      title={title}
+      variant='ghost'
+      onClick={() =>
+        dispatch(Help.actions.setHelpPosition(section, topic))
+      }
+    >
+      <HelpCircle className="h-6 w-6 m-auto"></HelpCircle>
+    </Button>
+  }
+)
+
+HelpLink.displayName = 'HelpLink';
+
+export { HelpLink };
