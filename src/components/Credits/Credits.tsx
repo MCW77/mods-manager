426b6fa2b6e7e96458dbb8a8b75418804ce17a79
MCW77 <michael.wisniewski77@gmail.com>
2024-03-16 11:18:00 +0100

    fix: counter tailwind css reset making svg block level
    Also replace css-files with tailwind

--- src/components/Credits/Credits.tsx
+++ src/components/Credits/Credits.tsx
@@ -1,13 +1,10 @@
 // react
 import React from 'react';
 
-// styles
-import './Credits.css';
-
 
 const Credits = React.memo(
-  () => 
-    <svg className={'credits'} viewBox="0 0 100 100">
+  () =>
+    <svg className={'inline h-[1em] fill-black stroke-[#eeca44] stroke-width-6'} viewBox="0 0 100 100">
       <path
         d="M 100 15.997 L 35.567 100 L 14.433 100 L 62.112 36.532 L 36.855 36.532 L 36.855 52.266 L 27.836 52.266 L 27.836 36.265 L 18.3 36.265 L 18.3 52.266 L 9.022 52.266 L 9.022 36.265 L 0 36.265 L 0 15.999 L 8.764 15.999 L 8.764 0 L 18.3 0 L 18.3 15.999 L 27.836 15.999 L 27.836 0 L 36.855 0 L 36.855 15.999 Z"></path>
     </svg>
