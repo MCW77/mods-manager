1a2e1b0fc0d76782648834200ed68432b0c1919a
MCW77 <michael.wisniewski77@gmail.com>
2024-04-04 04:36:15 +0200

    style: format code

--- src/domain/ModAssignment.ts
+++ src/domain/ModAssignment.ts
@@ -1,30 +1,18 @@
 // domain
-import { CharacterNames } from "../constants/characterSettings";
-import { GIMOFlatMod } from "./types/ModTypes";
+import { CharacterNames } from "#/constants/characterSettings";
 
-import { Mod } from "./Mod";
-import { OptimizationPlan } from "./OptimizationPlan";
-import { MissedGoals } from "./PlayerProfile";
-
-
-interface FlatModAssignment {
-  id: CharacterNames;
-  target: OptimizationPlan;
-  assignedMods: GIMOFlatMod[];
-  missedGoals: MissedGoals;
-  messages?: string[];
-
-}
+import { Mod } from "#/domain/Mod";
+import { OptimizationPlan } from "#/domain/OptimizationPlan";
+import { MissedGoals } from "#/domain/PlayerProfile";
 
 interface ModAssignment {
-  id: CharacterNames;
-  target: OptimizationPlan;
-  assignedMods: Mod[];
-  missedGoals: MissedGoals;
-  messages?: string[];
+	id: CharacterNames;
+	target: OptimizationPlan;
+	assignedMods: Mod[];
+	missedGoals: MissedGoals;
+	messages?: string[];
 }
 
 type ModAssignments = ModAssignment[];
-
 
 export type { ModAssignment, ModAssignments };
