53949f9fc5ffa8d8ceb75bcaa6679ab288f275c4
MCW77 <michael.wisniewski77@gmail.com>
2024-05-24 23:40:39 +0200

    refactor: make linter happy

--- src/constants/optimizationStrategy.js
+++ src/constants/optimizationStrategy.js
@@ -1,230 +1,230 @@
 import { createOptimizationPlan } from "../domain/OptimizationPlan";
 
 const optimizationStrategy = {
-  "Speed with survivability": createOptimizationPlan(
-    'Speed with survivability',
-    5, // health
-    5, // protection
-    100, // speed
-    0, // crit damage
-    0, // potency
-    5, // tenacity
-    0, // physical damage
-    0, // special damage
-    0, // crit chance
-    0, // armor
-    0, // resistance
-    0, // accuracy
-    0 // crit avoidance
-  ),
-  "Speed": createOptimizationPlan(
-    'Speed',
-    0, // health
-    0, // protection
-    100, // speed
-    0, // crit damage
-    0, // potency
-    0, // tenacity
-    0, // physical damage
-    0, // special damage
-    0, // crit chance
-    0, // armor
-    0, // resistance
-    0, // accuracy
-    0 // crit avoidance
-  ),
-  "Speed, Crit, and Physical Damage": createOptimizationPlan(
-    'Speed, Crit, and Physical Damage',
-    0, // health
-    0, // protection
-    100, // speed
-    100, // crit damage
-    0, // potency
-    0, // tenacity
-    50, // physical damage
-    0, // special damage
-    50, // crit chance
-    0, // armor
-    0, // resistance
-    0, // accuracy
-    0 // crit avoidance
-  ),
-  "Speed, Crit, and Special Damage": createOptimizationPlan(
-    'Speed, Crit, and Special Damage',
-    0, // health
-    0, // protection
-    100, // speed
-    100, // crit damage
-    0, // potency
-    0, // tenacity
-    50, // physical damage
-    0, // special damage
-    50, // crit chance
-    0, // armor
-    0, // resistance
-    0, // accuracy
-    0 // crit avoidance
-  ),
-  "Speed, Crit, and Mixed Damage": createOptimizationPlan(
-    'Speed, Crit, and Mixed Damage',
-    0, // health
-    0, // protection
-    100, // speed
-    100, // crit damage
-    0, // potency
-    0, // tenacity
-    50, // physical damage
-    50, // special damage
-    50, // crit chance
-    0, // armor
-    0, // resistance
-    0, // accuracy
-    0 // crit avoidance
-  ),
-  "Speed, Crit, Physical Damage, Potency": createOptimizationPlan(
-    'Speed, Crit, Physical Damage, Potency',
-    0, // health
-    0, // protection
-    100, // speed
-    100, // crit damage
-    25, // potency
-    0, // tenacity
-    50, // physical damage
-    0, // special damage
-    50, // crit chance
-    0, // armor
-    0, // resistance
-    0, // accuracy
-    0 // crit avoidance
-  ),
-  "Speed, Crit, Special Damage, Potency": createOptimizationPlan(
-    'Speed, Crit, Special Damage, Potency',
-    0, // health
-    0, // protection
-    100, // speed
-    100, // crit damage
-    25, // potency
-    0, // tenacity
-    0, // physical damage
-    50, // special damage
-    50, // crit chance
-    0, // armor
-    0, // resistance
-    0, // accuracy
-    0 // crit avoidance
-  ),
-  "Speed, Crit, Mixed Damage, Potency": createOptimizationPlan(
-    'Speed, Crit, Mixed Damage, Potency',
-    0, // health
-    0, // protection
-    100, // speed
-    100, // crit damage
-    25, // potency
-    0, // tenacity
-    50, // physical damage
-    50, // special damage
-    50, // crit chance
-    0, // armor
-    0, // resistance
-    0, // accuracy
-    0 // crit avoidance
-  ),
-  "Speedy debuffer": createOptimizationPlan(
-    'Speedy debuffer',
-    0, // health
-    0, // protection
-    100, // speed
-    0, // crit damage
-    25, // potency
-    0, // tenacity
-    0, // physical damage
-    0, // special damage
-    0, // crit chance
-    0, // armor
-    0, // resistance
-    0, // accuracy
-    0 // crit avoidance
-  ),
-  "Slow Crit, Physical Damage, Potency": createOptimizationPlan(
-    'Slow Crit, Physical Damage, Potency',
-    0, // health
-    0, // protection
-    10, // speed
-    100, // crit damage
-    25, // potency
-    0, // tenacity
-    50, // physical damage
-    0, // special damage
-    50, // crit chance
-    0, // armor
-    0, // resistance
-    0, // accuracy
-    0 // crit avoidance
-  ),
-  "Speedy Chex Mix": createOptimizationPlan(
-    'Speedy Chex Mix',
-    0, // health
-    0, // protection,
-    50, // speed
-    0, // crit damage
-    0, // potency
-    0, // tenacity,
-    100, // physical damage
-    0, // special damage
-    0, // crit chance
-    0, // armor
-    0, // resistance
-    0, // accuracy
-    0 // crit avoidance
-  ),
-  "Special Damage": createOptimizationPlan(
-    'Special Damage',
-    0, // health
-    0, // protection,
-    100, // speed
-    0, // crit damage
-    0, // potency
-    0, // tenacity,
-    0, // physical damage
-    50, // special damage
-    0, // crit chance
-    0, // armor
-    0, // resistance
-    0, // accuracy
-    0 // crit avoidance
-  ),
-  "Mixed Damage": createOptimizationPlan(
-    'Mixed Damage',
-    0, // health
-    0, // protection,
-    100, // speed
-    0, // crit damage
-    0, // potency
-    0, // tenacity,
-    50, // physical damage
-    50, // special damage
-    0, // crit chance
-    0, // armor
-    0, // resistance
-    0, // accuracy
-    0 // crit avoidance
-  ),
-  "Special Damage with Potency": createOptimizationPlan(
-    'Special Damage with Potency',
-    0, // health
-    0, // protection,
-    100, // speed
-    0, // crit damage
-    25, // potency
-    0, // tenacity,
-    0, // physical damage
-    50, // special damage
-    0, // crit chance
-    0, // armor
-    0, // resistance
-    0, // accuracy
-    0 // crit avoidance
-  )
+	"Speed with survivability": createOptimizationPlan(
+		"Speed with survivability",
+		5, // health
+		5, // protection
+		100, // speed
+		0, // crit damage
+		0, // potency
+		5, // tenacity
+		0, // physical damage
+		0, // special damage
+		0, // crit chance
+		0, // armor
+		0, // resistance
+		0, // accuracy
+		0, // crit avoidance
+	),
+	Speed: createOptimizationPlan(
+		"Speed",
+		0, // health
+		0, // protection
+		100, // speed
+		0, // crit damage
+		0, // potency
+		0, // tenacity
+		0, // physical damage
+		0, // special damage
+		0, // crit chance
+		0, // armor
+		0, // resistance
+		0, // accuracy
+		0, // crit avoidance
+	),
+	"Speed, Crit, and Physical Damage": createOptimizationPlan(
+		"Speed, Crit, and Physical Damage",
+		0, // health
+		0, // protection
+		100, // speed
+		100, // crit damage
+		0, // potency
+		0, // tenacity
+		50, // physical damage
+		0, // special damage
+		50, // crit chance
+		0, // armor
+		0, // resistance
+		0, // accuracy
+		0, // crit avoidance
+	),
+	"Speed, Crit, and Special Damage": createOptimizationPlan(
+		"Speed, Crit, and Special Damage",
+		0, // health
+		0, // protection
+		100, // speed
+		100, // crit damage
+		0, // potency
+		0, // tenacity
+		50, // physical damage
+		0, // special damage
+		50, // crit chance
+		0, // armor
+		0, // resistance
+		0, // accuracy
+		0, // crit avoidance
+	),
+	"Speed, Crit, and Mixed Damage": createOptimizationPlan(
+		"Speed, Crit, and Mixed Damage",
+		0, // health
+		0, // protection
+		100, // speed
+		100, // crit damage
+		0, // potency
+		0, // tenacity
+		50, // physical damage
+		50, // special damage
+		50, // crit chance
+		0, // armor
+		0, // resistance
+		0, // accuracy
+		0, // crit avoidance
+	),
+	"Speed, Crit, Physical Damage, Potency": createOptimizationPlan(
+		"Speed, Crit, Physical Damage, Potency",
+		0, // health
+		0, // protection
+		100, // speed
+		100, // crit damage
+		25, // potency
+		0, // tenacity
+		50, // physical damage
+		0, // special damage
+		50, // crit chance
+		0, // armor
+		0, // resistance
+		0, // accuracy
+		0, // crit avoidance
+	),
+	"Speed, Crit, Special Damage, Potency": createOptimizationPlan(
+		"Speed, Crit, Special Damage, Potency",
+		0, // health
+		0, // protection
+		100, // speed
+		100, // crit damage
+		25, // potency
+		0, // tenacity
+		0, // physical damage
+		50, // special damage
+		50, // crit chance
+		0, // armor
+		0, // resistance
+		0, // accuracy
+		0, // crit avoidance
+	),
+	"Speed, Crit, Mixed Damage, Potency": createOptimizationPlan(
+		"Speed, Crit, Mixed Damage, Potency",
+		0, // health
+		0, // protection
+		100, // speed
+		100, // crit damage
+		25, // potency
+		0, // tenacity
+		50, // physical damage
+		50, // special damage
+		50, // crit chance
+		0, // armor
+		0, // resistance
+		0, // accuracy
+		0, // crit avoidance
+	),
+	"Speedy debuffer": createOptimizationPlan(
+		"Speedy debuffer",
+		0, // health
+		0, // protection
+		100, // speed
+		0, // crit damage
+		25, // potency
+		0, // tenacity
+		0, // physical damage
+		0, // special damage
+		0, // crit chance
+		0, // armor
+		0, // resistance
+		0, // accuracy
+		0, // crit avoidance
+	),
+	"Slow Crit, Physical Damage, Potency": createOptimizationPlan(
+		"Slow Crit, Physical Damage, Potency",
+		0, // health
+		0, // protection
+		10, // speed
+		100, // crit damage
+		25, // potency
+		0, // tenacity
+		50, // physical damage
+		0, // special damage
+		50, // crit chance
+		0, // armor
+		0, // resistance
+		0, // accuracy
+		0, // crit avoidance
+	),
+	"Speedy Chex Mix": createOptimizationPlan(
+		"Speedy Chex Mix",
+		0, // health
+		0, // protection,
+		50, // speed
+		0, // crit damage
+		0, // potency
+		0, // tenacity,
+		100, // physical damage
+		0, // special damage
+		0, // crit chance
+		0, // armor
+		0, // resistance
+		0, // accuracy
+		0, // crit avoidance
+	),
+	"Special Damage": createOptimizationPlan(
+		"Special Damage",
+		0, // health
+		0, // protection,
+		100, // speed
+		0, // crit damage
+		0, // potency
+		0, // tenacity,
+		0, // physical damage
+		50, // special damage
+		0, // crit chance
+		0, // armor
+		0, // resistance
+		0, // accuracy
+		0, // crit avoidance
+	),
+	"Mixed Damage": createOptimizationPlan(
+		"Mixed Damage",
+		0, // health
+		0, // protection,
+		100, // speed
+		0, // crit damage
+		0, // potency
+		0, // tenacity,
+		50, // physical damage
+		50, // special damage
+		0, // crit chance
+		0, // armor
+		0, // resistance
+		0, // accuracy
+		0, // crit avoidance
+	),
+	"Special Damage with Potency": createOptimizationPlan(
+		"Special Damage with Potency",
+		0, // health
+		0, // protection,
+		100, // speed
+		0, // crit damage
+		25, // potency
+		0, // tenacity,
+		0, // physical damage
+		50, // special damage
+		0, // crit chance
+		0, // armor
+		0, // resistance
+		0, // accuracy
+		0, // crit avoidance
+	),
 };
 
 Object.freeze(optimizationStrategy);
--- src/domain/types/ModTypes.ts
+++ src/domain/types/ModTypes.ts
@@ -2,12 +2,39 @@ import type { CharacterNames } from "#/constants/characterSettings";
 import type { ModTiersEnum } from "../../constants/enums";
 import type { SetStats, PrimaryStats, SecondaryStats } from "../Stats";
 
-export type Levels = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15;
+export type Levels =
+	| 1
+	| 2
+	| 3
+	| 4
+	| 5
+	| 6
+	| 7
+	| 8
+	| 9
+	| 10
+	| 11
+	| 12
+	| 13
+	| 14
+	| 15;
 export type Pips = 1 | 2 | 3 | 4 | 5 | 6;
-export type StrPips = '1' | '2' | '3' | '4' | '5' | '6';
-export type GIMOSlots = 'square' | 'arrow' | 'diamond' | 'triangle' | 'circle' | 'cross';
-export type HUSlots = 'Transmitter' | 'Receiver' | 'Processor' | 'Holo-Array' | 'Data-Bus' | 'Multiplexer';
-export type VariablePrimarySlots = 'arrow' | 'triangle' | 'circle' | 'cross';
+export type StrPips = "1" | "2" | "3" | "4" | "5" | "6";
+export type GIMOSlots =
+	| "square"
+	| "arrow"
+	| "diamond"
+	| "triangle"
+	| "circle"
+	| "cross";
+export type HUSlots =
+	| "Transmitter"
+	| "Receiver"
+	| "Processor"
+	| "Holo-Array"
+	| "Data-Bus"
+	| "Multiplexer";
+export type VariablePrimarySlots = "arrow" | "triangle" | "circle" | "cross";
 
 type t2 = 1 | 2 | 3 | 4;
 type t4 = `secondaryType_${t2}`;
@@ -15,68 +42,72 @@ type t5 = `secondaryValue_${t2}`;
 type t6 = `secondaryRoll_${t2}`;
 
 export type FlatHUModTypeIndexer = {
-  [key in t4]: SecondaryStats.HUStatNames | null;
+	[key in t4]: SecondaryStats.HUStatNames | null;
 };
 export type FlatGIMOModTypeIndexer = {
-  [key in t4]: SecondaryStats.GIMOStatNames | null;
+	[key in t4]: SecondaryStats.GIMOStatNames | null;
 };
 export type FlatModValueIndexer = {
-  [key in t5]: string | null;
+	[key in t5]: string | null;
 };
 export type FlatModRollIndexer = {
-  [key in t6]: SecondaryStats.StrRolls | null;
+	[key in t6]: SecondaryStats.StrRolls | null;
 };
 
 export type HUFlatMod = {
-  mod_uid: string,
-  primaryBonusType: PrimaryStats.HUStatNames,
-  primaryBonusValue: string,
+	mod_uid: string;
+	primaryBonusType: PrimaryStats.HUStatNames;
+	primaryBonusValue: string;
 
-  secondaryType_1: SecondaryStats.HUStatNames | null,
-  secondaryValue_1: string,
-  secondaryRoll_1: SecondaryStats.StrRolls | null,
-  secondaryType_2: SecondaryStats.HUStatNames | null,
-  secondaryValue_2: string,
-  secondaryRoll_2: SecondaryStats.StrRolls | null,
-  secondaryType_3: SecondaryStats.HUStatNames | null,
-  secondaryValue_3: string,
-  secondaryRoll_3: SecondaryStats.StrRolls | null,
-  secondaryType_4: SecondaryStats.HUStatNames | null,
-  secondaryValue_4: string,
-  secondaryRoll_4: SecondaryStats.StrRolls | null,
+	secondaryType_1: SecondaryStats.HUStatNames | null;
+	secondaryValue_1: string;
+	secondaryRoll_1: SecondaryStats.StrRolls | null;
+	secondaryType_2: SecondaryStats.HUStatNames | null;
+	secondaryValue_2: string;
+	secondaryRoll_2: SecondaryStats.StrRolls | null;
+	secondaryType_3: SecondaryStats.HUStatNames | null;
+	secondaryValue_3: string;
+	secondaryRoll_3: SecondaryStats.StrRolls | null;
+	secondaryType_4: SecondaryStats.HUStatNames | null;
+	secondaryValue_4: string;
+	secondaryRoll_4: SecondaryStats.StrRolls | null;
 
-  slot: HUSlots,
-  set: SetStats.HUStatNames,
-  level: Levels,
-  pips: Pips,
-  tier: ModTiersEnum,
-  characterID: CharacterNames | 'null',
-  reRolledCount: number,
-} & FlatHUModTypeIndexer & FlatModValueIndexer & FlatModRollIndexer
+	slot: HUSlots;
+	set: SetStats.HUStatNames;
+	level: Levels;
+	pips: Pips;
+	tier: ModTiersEnum;
+	characterID: CharacterNames | "null";
+	reRolledCount: number;
+} & FlatHUModTypeIndexer &
+	FlatModValueIndexer &
+	FlatModRollIndexer;
 
 export type GIMOFlatMod = {
-  mod_uid: string,
-  primaryBonusType: PrimaryStats.GIMOStatNames,
-  primaryBonusValue: string,
+	mod_uid: string;
+	primaryBonusType: PrimaryStats.GIMOStatNames;
+	primaryBonusValue: string;
 
-  secondaryType_1: SecondaryStats.GIMOStatNames | null,
-  secondaryValue_1: string,
-  secondaryRoll_1: SecondaryStats.StrRolls | null,
-  secondaryType_2: SecondaryStats.GIMOStatNames | null,
-  secondaryValue_2: string,
-  secondaryRoll_2: SecondaryStats.StrRolls | null,
-  secondaryType_3: SecondaryStats.GIMOStatNames | null,
-  secondaryValue_3: string,
-  secondaryRoll_3: SecondaryStats.StrRolls | null,
-  secondaryType_4: SecondaryStats.GIMOStatNames | null,
-  secondaryValue_4: string,
-  secondaryRoll_4: SecondaryStats.StrRolls | null,
+	secondaryType_1: SecondaryStats.GIMOStatNames | null;
+	secondaryValue_1: string;
+	secondaryRoll_1: SecondaryStats.StrRolls | null;
+	secondaryType_2: SecondaryStats.GIMOStatNames | null;
+	secondaryValue_2: string;
+	secondaryRoll_2: SecondaryStats.StrRolls | null;
+	secondaryType_3: SecondaryStats.GIMOStatNames | null;
+	secondaryValue_3: string;
+	secondaryRoll_3: SecondaryStats.StrRolls | null;
+	secondaryType_4: SecondaryStats.GIMOStatNames | null;
+	secondaryValue_4: string;
+	secondaryRoll_4: SecondaryStats.StrRolls | null;
 
-  slot: GIMOSlots,
-  set: SetStats.GIMOStatNames,
-  level: Levels,
-  pips: Pips,
-  tier: ModTiersEnum,
-  characterID: CharacterNames | 'null',
-  reRolledCount: number,
-} & FlatGIMOModTypeIndexer & FlatModValueIndexer & FlatModRollIndexer
+	slot: GIMOSlots;
+	set: SetStats.GIMOStatNames;
+	level: Levels;
+	pips: Pips;
+	tier: ModTiersEnum;
+	characterID: CharacterNames | "null";
+	reRolledCount: number;
+} & FlatGIMOModTypeIndexer &
+	FlatModValueIndexer &
+	FlatModRollIndexer;
