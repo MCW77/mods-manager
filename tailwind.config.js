28ad0c9cec0cb8faf6aab6d69697e7f166cc3db2
MCW77 <michael.wisniewski77@gmail.com>
2024-12-03 16:25:54 +0100

    chore: add index.html to tailwind.config content prop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+	darkMode: ["class"],
+	content: [
+		"./index.html",
+		"./pages/**/*.{ts,tsx}",
+		"./components/**/*.{ts,tsx}",
+		"./app/**/*.{ts,tsx}",
+		"./src/**/*.{ts,tsx}",
 	],
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-        "spin-slow": {
-          from: { transform: 'rotate(0deg)' },
-          to: { transform: 'rotate(360deg)' },
-        },
-        "spin-slow-reverse": {
-          from: { transform: 'rotate(360deg)' },
-          to: { transform: 'rotate(0deg)' },
-        }
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        "spin-slow": "spin-slow 2s linear infinite",
-        "spin-slow-reverse": "spin-slow-reverse 2s linear infinite",
-      },
-    },
-  },
-  plugins: [require("tailwindcss-animate")],
-}
+	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
+		extend: {
+			keyframes: {
+				"accordion-down": {
+					from: { height: 0 },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: 0 },
+				},
+				"spin-slow": {
+					from: { transform: "rotate(0deg)" },
+					to: { transform: "rotate(360deg)" },
+				},
+				"spin-slow-reverse": {
+					from: { transform: "rotate(360deg)" },
+					to: { transform: "rotate(0deg)" },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+				"spin-slow": "spin-slow 2s linear infinite",
+				"spin-slow-reverse": "spin-slow-reverse 2s linear infinite",
+			},
+		},
+	},
+	plugins: [require("tailwindcss-animate")],
+};
