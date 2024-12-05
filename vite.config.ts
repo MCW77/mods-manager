6b1e470438b8defa8ec858469a716e1fe0f15280
MCW77 <michael.wisniewski77@gmail.com>
2024-12-05 12:15:04 +0100

    chore: add vite-plugin-dynamic-import to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import UnoCSS from "unocss/vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 import path from "node:path";
+import dynamicImport from "vite-plugin-dynamic-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -30,6 +31,7 @@ export default defineConfig({
 */
 	},
 	plugins: [
+		dynamicImport(),
 		react(),
 		VitePWA({
 			includeAssets: [
