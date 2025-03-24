effe83d8e63b856d8088315fc7c26d10c1ecae70
MCW77 <michael.wisniewski77@gmail.com>
2025-03-24 18:19:55 +0100

    chore: replace old fisand preset.shadcn with actively maintained version by hyoban

--- uno.config.ts
+++ uno.config.ts
@@ -5,49 +5,49 @@ import {
 	presetTypography,
 } from "unocss";
 import presetAnimations from "unocss-preset-animations";
-import presetShadcn from "./src/lib/shadcn/preset.shadcn";
+import presetShadcn from "unocss-preset-shadcn";
 
 export default defineConfig({
-  presets: [
+	presets: [
 		presetWind3(),
-    presetAnimations(),
-    presetIcons(),
-    presetShadcn(),
-    presetTypography(),
-  ],
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
-        },
-        "spin-slow": {
+		presetAnimations(),
+		presetIcons(),
+		presetShadcn(),
+		presetTypography(),
+	],
+	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
+		extend: {
+			keyframes: {
+				"accordion-down": {
+					from: { height: 0 },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: 0 },
+				},
+				"spin-slow": {
 					from: { transform: "rotate(0deg)" },
 					to: { transform: "rotate(360deg)" },
-        },
-        "spin-slow-reverse": {
+				},
+				"spin-slow-reverse": {
 					from: { transform: "rotate(360deg)" },
 					to: { transform: "rotate(0deg)" },
 				},
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        "spin-slow": "spin-slow 2s linear infinite",
-        "spin-slow-reverse": "spin-slow-reverse 2s linear infinite",
-      },
-    },
-  },
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+				"spin-slow": "spin-slow 2s linear infinite",
+				"spin-slow-reverse": "spin-slow-reverse 2s linear infinite",
+			},
+		},
+	},
 });
