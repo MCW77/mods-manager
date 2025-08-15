af20d383df9e78a8dfbaeb6c51bb0ca815d51072
MCW77 <michael.wisniewski77@gmail.com>
2025-08-15 21:07:42 +0200

    test: add fixtures (-loader) for use in tests

---
+++ tests/migrations/fixtures/backup/v16.ts
@@ -0,0 +1,7 @@
+export const defaultFixture = {
+	// Pre-v18 backup structure - matches ModsManagerBackupSchemaV16
+	characterTemplates: {},
+	lastRuns: [],
+	profilesManagement: {},
+	version: "1.0.0", // This will be transformed by the schema
+};
