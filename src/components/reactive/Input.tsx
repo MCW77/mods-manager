b8b3d66843e28fef79993207b45809b2b1be1abc
MCW77 <7250892+MCW77@users.noreply.github.com>
2026-01-13 02:26:13 +0100

    fix: handle all kind of observables
    - computeds
    - checkbox/radio input

--- src/components/reactive/Input.tsx
+++ src/components/reactive/Input.tsx
@@ -3,7 +3,7 @@ import { useMemo, type ComponentProps } from "react";
 import { useValue } from "@legendapp/state/react";
 
 // state
-import { observable, type Observable } from "@legendapp/state";
+import { observable, type ObservableParam } from "@legendapp/state";
 
 // components
 import { Input as ShadCNInput } from "#ui/input";
@@ -13,32 +13,46 @@ import { numberAsString } from "#/utils/legend-kit/as/numberAsString";
 type CustomInputProps = ComponentProps<typeof ShadCNInput>;
 interface InputProps
 	extends Omit<CustomInputProps, "checked" | "value" | "onChange"> {
-	$value: Observable<string | number | boolean | undefined>;
+	$value: ObservableParam<string | number | boolean | undefined>;
 	onChange?: (value: string | undefined) => void;
 }
 function Input({ type, $value, onChange, ...props }: InputProps) {
 	const value$ = useMemo(() => {
 		if (type === "checkbox" || type === "radio")
 			return observable(
-				booleanAsString($value as Observable<boolean | undefined>),
+				booleanAsString($value as ObservableParam<boolean | undefined>),
 			);
 		if (type === "number")
 			return observable(
-				numberAsString($value as Observable<number | undefined>),
+				numberAsString($value as ObservableParam<number | undefined>),
 			);
 		if ($value === undefined) return observable<string>("");
-		return $value as Observable<string | undefined>;
+		return $value as ObservableParam<string | undefined>;
 	}, [$value, type]);
 	const value = useValue(value$);
 
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		const newValue = event.target.value;
+		const newValue =
+			type === "checkbox" || type === "radio"
+				? event.target.checked
+					? "true"
+					: "false"
+				: event.target.value;
 		value$.set(newValue);
 		onChange?.(newValue);
 	};
 
+	const isCheckboxOrRadio = type === "checkbox" || type === "radio";
+
 	return (
-		<ShadCNInput {...props} onChange={handleChange} type={type} value={value} />
+		<ShadCNInput
+			{...props}
+			onChange={handleChange}
+			type={type}
+			{...(isCheckboxOrRadio
+				? { checked: value === "true" }
+				: { value: value })}
+		/>
 	);
 }
 
