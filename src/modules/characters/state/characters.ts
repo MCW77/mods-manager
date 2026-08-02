8cada7cd92e0f8ed7684928d675440839fe8ac67
MCW77 <7250892+MCW77@users.noreply.github.com>
2026-08-02 15:27:31 +0200

    fix: avoid web worker crashing
    character.ts is indirectly imported by the web worker optimizer.ts.
    With HMR activated a web worker throws an error if importing a component.
    The crash is avoided by dynamically importing the dialog module,
     which brings the component, only if not in a web worker.

--- src/modules/characters/state/characters.ts
+++ src/modules/characters/state/characters.ts
@@ -3,8 +3,6 @@ import { observable } from "@legendapp/state";
 import { syncObservable } from "@legendapp/state/sync";
 import { persistOptions } from "#/utils/globalLegendPersistSettings";
 
-import { dialog$ } from "#/modules/dialog/state/dialog";
-
 // api
 import { fetchCharacters } from "../api/fetchBaseCharacters";
 
@@ -13,17 +11,35 @@ import type { BaseCharacterById } from "../domain/BaseCharacter";
 import type { CharactersObservable } from "../domain/CharactersObservable";
 import { BaseError } from "#/domain/BaseError";
 
+const insideWebWorker = !Object.hasOwn(self, "document");
+
+const dialogModule =
+	insideWebWorker === false
+		? await import("#/modules/dialog/state/dialog")
+		: undefined;
+
 const characters$ = observable<CharactersObservable>({
 	baseCharacterById: async () => {
 		try {
 			return await fetchCharacters();
 		} catch (error) {
-			if (error instanceof BaseError) {
-				dialog$.showError({
+			if (error instanceof BaseError && dialogModule !== undefined) {
+				dialogModule.dialog$.showError({
 					error: error.error,
 					reason: error.reason,
 					solution: error.solution,
 				});
+			} else {
+				if (error instanceof BaseError) {
+					console.error(error.error);
+					console.error(`Reason: ${error.reason}`);
+					console.error(`Solution: ${error.solution}`);
+				} else {
+					console.error(
+						"Unexpected error while fetching base characters:",
+						error,
+					);
+				}
 			}
 			return {} as BaseCharacterById; // Return an empty object to satisfy the return type
 		}
