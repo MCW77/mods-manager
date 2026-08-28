fc30d78b8f2341cb20cc31db9e95ff73f7454867
MCW77 <7250892+MCW77@users.noreply.github.com>
2026-08-28 17:43:19 +0200

    refactor: make linter happy

--- src/components/ui/charts.tsx
+++ src/components/ui/charts.tsx
@@ -299,7 +299,7 @@ function ChartLegendContent({
 		>
 			{payload
 				.filter((item) => item.type !== "none")
-				.map((item, index) => {
+				.map((item, _index) => {
 					const key = `${nameKey ?? item.dataKey ?? "value"}`;
 					const itemConfig = getPayloadConfigFromPayload(config, item, key);
 
