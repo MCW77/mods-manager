6c6944f88dd2dd6527aaab4376a1425c8e9561a9
MCW77 <7250892+MCW77@users.noreply.github.com>
2026-09-03 03:53:10 +0200

    feat: allow statistics view to scoll when overflowing

--- src/modules/statistics/pages/StatisticsView.tsx
+++ src/modules/statistics/pages/StatisticsView.tsx
@@ -6,20 +6,23 @@ import { ModSlots } from "../components/ModSlots";
 import { ModsToFarm } from "../components/ModsToFarm";
 import { OffenseDefenseCard } from "../components/OffenseDefenseCard";
 import { SpeedCard } from "../components/SpeedCard";
+import { ScrollArea } from "#ui/scroll-area";
 
 function StatisticsView() {
 	return (
-		<div className="flex flex-wrap gap-4 p-4 w-full items-start">
-			<SpeedCard />
-			<div className="flex flex-col gap-4">
-				<OffenseDefenseCard />
-				<ModQualityCard />
+		<ScrollArea>
+			<div className="flex flex-wrap gap-4 p-4 w-full items-start">
+				<SpeedCard />
+				<div className="flex flex-col gap-4">
+					<OffenseDefenseCard />
+					<ModQualityCard />
+				</div>
+				<ModsetsCard />
+				<ModSlots />
+				<ModsToFarm />
+				<MissingModsOfSlots />
 			</div>
-			<ModsetsCard />
-			<ModSlots />
-			<ModsToFarm />
-			<MissingModsOfSlots />
-		</div>
+		</ScrollArea>
 	);
 }
 
