8a2fb2a02ceaf1acc92f2cf65a65f5a813826c00
MCW77 <7250892+MCW77@users.noreply.github.com>
2026-09-25 06:26:40 +0200

    test: remove the lint error to make the PR pass CI

--- src/workers/optimizer.ts
+++ src/workers/optimizer.ts
@@ -76,7 +76,6 @@ import type { MissedGoals } from "../modules/compilations/domain/MissedGoals";
 import type { ProfileOptimizationSettings } from "../modules/optimizationSettings/domain/ProfileOptimizationSettings";
 import type { WithoutCC } from "#/domain/CharacterStatNames";
 
-const linterror = 4;
 // #region types
 interface Cache {
 	modsetScore: {
