017c9d283a9b76e068f8e399b06acfb590636611
MCW77 <7250892+MCW77@users.noreply.github.com>
2026-09-25 05:02:35 +0200

    test: check again with updated rules and jj config
    - branch protection now requires linear history
    - JJ is configured to do a pr on a push

--- src/workers/optimizer.ts
+++ src/workers/optimizer.ts
@@ -76,7 +76,7 @@ import type { MissedGoals } from "../modules/compilations/domain/MissedGoals";
 import type { ProfileOptimizationSettings } from "../modules/optimizationSettings/domain/ProfileOptimizationSettings";
 import type { WithoutCC } from "#/domain/CharacterStatNames";
 
-const linterror = 2;
+const linterror = 3;
 // #region types
 interface Cache {
 	modsetScore: {
